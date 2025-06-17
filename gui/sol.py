8044c7e83dc7306638bd56522c7ee55c697411ec
Shamil Nabiyev <shamilnabiyev@users.noreply.github.com>
2025-06-17 20:54:30 +0200

    Code refactoring

--- gui/sol.py
+++ gui/sol.py
@@ -1,79 +1,47 @@
-import os
 import solara
 import solara.lab
-import asyncio
-from typing import cast
 from typing import List
 from copy import deepcopy
-from openai import OpenAI
 from functools import partial
-from dotenv import load_dotenv
-from openai.types.chat import ChatCompletionMessageParam
 from typing_extensions import TypedDict
+from utils.llm import (
+    find_sql, 
+    generate_sql, 
+    init_messages
+)
 
-from openai import AzureOpenAI
-from azure.identity import DefaultAzureCredential
-from azure.identity import get_bearer_token_provider
-
-load_dotenv('.env.dev', override=True)
+COLUMN_STYLE = {
+    "width": "100%",
+    "height": "95%",
+    "border": "1px solid #e0e0e0",           
+    "borderRadius": "12px",                   
+    "boxShadow": "0 4px 16px rgba(0,0,0,0.07)", 
+    "padding": "24px",               
+    "backgroundColor": "#fff",                
+}
 
 class MessageDict(TypedDict):
     role: str  # "user" or "assistant"
     content: str
-    is_contains_sql: bool
+    is_sql_statement: bool
     is_end_of_stream: bool
 
-with open("db/prompt/system.txt", "r") as file:
-    SYSTEM_PROMPT = file.read()
-
-with open("db/prompt/sqlcheck.txt", "r") as file:
-    SQL_CHECK_PROMPT = file.read()
 
 messages: solara.Reactive[List[MessageDict]] = solara.reactive([
-    {"role": "system", "content": SYSTEM_PROMPT}
+    init_messages()
 ])
 
-token_provider = get_bearer_token_provider(
-    DefaultAzureCredential(), 
-    "https://cognitiveservices.azure.com/.default"
-)
-
-AZURE_OPENAI_MODEL_DEPLOYMENT = os.getenv("AZURE_OPENAI_MODEL_DEPLOYMENT")
-
-openai_client = AzureOpenAI(
-    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
-    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-    azure_ad_token_provider=token_provider
-)
 
 def store_feedback(reaction, user_input, chatbot_answer):
     print(reaction, user_input, chatbot_answer)
 
-
-def contains_sql(text):
-    turn_message = [
-        {"role": "system", "content": SQL_CHECK_PROMPT},
-        {"role": "user", "content": f"<input>{text}</input>"},
-    ]
-
-    response = openai_client.chat.completions.create(
-        messages=turn_message,
-        max_tokens=2,
-        temperature=0.0,
-        top_p=1.0,
-        model=AZURE_OPENAI_MODEL_DEPLOYMENT,
-        stream=False,
-        stop=["\n"],
-    )
-    result = response.choices[0].message.content.strip().lower()
-    return result == "true"
 
 def create_system_message():
     return {
         "role": "assistant", 
         "content": "", 
         "is_end_of_stream": False, 
-        "is_contains_sql": False
+        "is_sql_statement": False
     }
 
 def run_query(text):
@@ -94,15 +62,7 @@ async def promt_ai(message: str):
     else:
         turn_messages = deepcopy([messages.value[0], *messages.value[-(CONTEXT_LENGTH-1):]])
     
-    response = openai_client.chat.completions.create(
-        messages=turn_messages,
-        max_tokens=256,
-        temperature=0.75,
-        top_p=1.0,
-        model=AZURE_OPENAI_MODEL_DEPLOYMENT,
-        stream=True,
-        stop=[";"],
-    )
+    response = generate_sql(turn_messages)
 
     messages.value = [*messages.value, create_system_message()]
 
@@ -125,8 +85,8 @@ async def promt_ai(message: str):
         # replace the last message element with the appended content which will update the UI
         messages.value = [*messages.value[:-1], updated_message]
     
-    if contains_sql(messages.value[-1].get('content', '')) == True:
-        messages.value[-1]['is_contains_sql'] = True
+    if find_sql(messages.value[-1].get('content', '')) == True:
+        messages.value[-1]['is_sql_statement'] = True
     
     return
 
@@ -134,16 +94,9 @@ async def promt_ai(message: str):
 @solara.component
 def Page():
     with solara.Column(
-        style={
-            "width": "100%",
-            "height": "95%",
-            "border": "1px solid #e0e0e0",           
-            "borderRadius": "12px",                   
-            "boxShadow": "0 4px 16px rgba(0,0,0,0.07)", 
-            "padding": "24px",               
-            "backgroundColor": "#fff",                
-        }
+        style=COLUMN_STYLE
     ):
+        solara.Title("Solara SQL Chatbot")
         with solara.lab.ChatBox():
             for i, item in enumerate(messages.value):
                 if (item["role"] == "system"):
@@ -169,21 +122,21 @@ def Page():
                         ): 
                             if item.get('is_end_of_stream', False):
                                 solara.Button(
-                                    label="like", 
+                                    label="", 
                                     icon_name="mdi-thumb-up",
                                     outlined=True,
                                     color="primary",
                                     on_click=partial(store_feedback, "like", user_message, item),
                                 )
                                 solara.Button(
-                                    label="dislike", 
+                                    label="", 
                                     outlined=True, 
                                     color="primary",
                                     icon_name="mdi-thumb-down",
                                     on_click=partial(store_feedback, "dislike", user_message, item),
                                 )
 
-                            if item.get('is_contains_sql', False):
+                            if item.get('is_sql_statement', False):
                                 solara.Button(
                                     label="run query", 
                                     outlined=True,
